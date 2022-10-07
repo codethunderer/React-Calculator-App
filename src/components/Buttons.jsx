bfa6272c856cc9bb146ccbd38085fa323669a90c
Faith Odukudu <faithodukudu@gmail.com>
2022-10-07 14:39:49 +0000

    Initial commit

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./styles/Buttons.css";
+// import CALCULATOR_BUTTONS from "./CalculatorButtons";
+
+const Buttons = ({ inputHandler, clearInput, backspace, calculateAns }) => {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("equalbtn").click();
+    }
+  });
+
+  return (
+    <div className="show-btn">
+      <button className="btn clr" onClick={clearInput}>
+        DEL
+      </button>
+      <button className="btn clr" onClick={backspace}>
+        ⌫
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        0
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        /
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        7
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        8
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        9
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        *
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        4
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        5
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        6
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        -
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        1
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        2
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        3
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        +
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        .
+      </button>
+      <button className="btn exp equal" id="equalbtn" onClick={calculateAns}>
+        =
+      </button>
+    </div>
+  );
+};
+
+export default Buttons;
